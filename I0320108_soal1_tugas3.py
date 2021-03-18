8c37b9e5890948414ed3b70bf048bf6eac942ade
Vincentius Maurich <maurichkian@gmail.com>
2021-03-18 15:18:17 +0700

    Soal 1-2

--- I0320108_soal1_tugas3.py
+++ I0320108_soal1_tugas3.py
@@ -1,1 +1,20 @@
+#Membuat List Teman
+teman = ['Aji', 'Alica', 'Alvin', 'Diva', 'Rana', 'Raysa', 'Rio', 'Sasa', 'Sekar', 'Stefany']
 
+#Menampilkan Nama Teman Urutan 4,6,7
+print("Nama teman indeks 4,6,7: ", teman[4], ",", teman[6], ",", teman[-3])
+
+#Mengganti Nama Teman Urutan 3,5,9
+teman[2] = 'Yola'
+teman[4] = 'Yuku'
+teman[-2] = 'Zafira'
+
+#Menambahkan 2 Nama Teman
+teman.append('Tsania')
+teman.append('Zedi')
+
+#Menampilkan Nama Teman dengan Perulangan
+print(teman*2)
+
+#Menampilkan Panjang List
+print(len(teman))
--- I0320108_soal2_tugas3.py
+++ I0320108_soal2_tugas3.py
@@ -1,1 +1,18 @@
+#Membuat Dictionary
+dict = {'Nama': 'Maurich',
+        'Hobi': ['mendengarkan lagu', 'makan'],
+        'Sosial Media': ['Line','Instagram','Twitter','linkedIn'],
+        'Lagu Kesukaan': ['Higher', 'Mariposa', 'Supercuts'],
+        'Makanan Favorit': ['ayam goreng', 'mie ayam', 'batagor']}
 
+#Mengubah Salah Satu Hobi dan Sosial Media
+dict['Hobi'][1]=('bernyanyi')
+dict['Sosial Media'][3]=('WhatsApp')
+
+#Menghapus Dua Makanan Favorit
+del dict['Makanan Favorit'][0:2]
+
+#Menambahkan Satu Hobi
+dict['Hobi'].append('tidur')
+
+print(dict)
